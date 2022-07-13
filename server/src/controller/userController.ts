fdfbd7adbc9edd740907bc96d61b52d918d6471e
alejapiatti <104015514+alejapiatti@users.noreply.github.com>
2022-07-13 11:00:02 +0200

    #7 acabado con test

--- server/src/controller/userController.ts
+++ server/src/controller/userController.ts
@@ -8,11 +8,13 @@ const userController =  async (req:Request,res:Response)=>{
         const {email,password,name,last_name,role, ...users}:iUser = req.body;
         if(!email || !password || !name || !last_name || !role){
             res.status(400).json({message:'email, password, name, last name or role missing'});
-        }
-        const result = await userModel.saveUser({email,password,name,last_name,role, ...users});
+        } else {
+            const result = await userModel.saveUser({email,password,name,last_name,role, ...users});
         result
                 ? res.status(201).json({ result: result.rows})
                 : res.status(500).send('No se pudo crear un nuevo usuario');
+        }
+        
     }catch (error: any){
         res.status(400).send(error.message);
     }
