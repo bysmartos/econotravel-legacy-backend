b3ade7d3768644d8a984471ccad2b41486a1197c
nuria-barea <nuriab@gmail.com>
2022-07-18 12:47:23 +0200

    #13 get/reservas modified with authorization

--- server/__tests__/routes/reservas.test.ts
+++ server/__tests__/routes/reservas.test.ts
@@ -11,6 +11,7 @@ describe('/reservas', ()=>
     test('should return status 200 & content-type "application/json"',async ()=>{
         const response = await request(app)
         .get('/reservas')
+        .set('Authorization', `Bearer eyJhbGciOiJIUzI1NiJ9.ZWR1ckBtYWlsLmNvbQ.UqqO6APmGNwy3fC-tl_qdeCNMkaCOizjeHk5btr_KOI`);
         expect(response.status).toBe(200);
         expect(response.headers['content-type']).toContain('application/json')
     }))
