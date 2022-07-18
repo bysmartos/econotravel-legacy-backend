1fe07ea3a9bff3a8fc61831c97143cea98d42ffb
nuria-barea <nuriab@gmail.com>
2022-07-18 15:53:48 +0200

    #10 role autorization implemented and run build

--- server/build/route/userRoute.js
+++ server/build/route/userRoute.js
@@ -6,9 +6,10 @@ exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var userController_1 = __importDefault(require("../controller/userController"));
 var auth_1 = __importDefault(require("../middlewares/auth"));
+var jwtHandler_1 = __importDefault(require("../middlewares/jwtHandler"));
 var router = (0, express_1["default"])();
 router.post('/user/register', auth_1["default"].encryptPassword, userController_1["default"].saveUser);
-router.get('/user/all', userController_1["default"].getAllUsers);
+router.get('/user/all', jwtHandler_1["default"].validateTokenRole, userController_1["default"].getAllUsers);
 router.post('/user/login', auth_1["default"].validateUser, userController_1["default"].login);
 exports["default"] = router;
 /*router.post('/user',authHandler.encryptPassword,userController.saveUser);   router.get('/user',validateToken,userController.getAllUsers); */ 
