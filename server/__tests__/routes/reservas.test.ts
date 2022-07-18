aca56e615b978f20c0dd1360538d37c34bff2531
nuria-barea <nuriab@gmail.com>
2022-07-18 15:46:10 +0200

    #13 testing done

--- server/__tests__/routes/reservas.test.ts
+++ server/__tests__/routes/reservas.test.ts
@@ -16,4 +16,29 @@ describe('/reservas', ()=>
         expect(response.headers['content-type']).toContain('application/json')
     }))
 
+    describe("POST /reservas/:userId", () => {
+        describe("when passed all info", () => {
+            test('should respond with a 200 & content-type "application/json', async () => {
+                const response = await request(app)
+                    .post('/reservas/2')
+                    .send({
+                        experienciaId:"4"
+                    }).set('Authorization', `Bearer eyJhbGciOiJIUzI1NiJ9.c2FuQG1haWwuY29t.2DZNZcVD-R3oJPACqFLNClrXv9r_cT6-6lUjsl_gGAE`);
+                expect(response.status).toEqual(201);
+                expect(response.headers['content-type']).toContain('application/json');
+            })
+            // should save the username and password in the database
+            // should respond with a json object that contains the id from the database. (probably jwt in the real world)
+        })
+        describe("when some info is missing", () => {
+            test("should return a 400 status code to show there was a user error.", async () => {
+                const response = await request(app)
+                    .post('/reservas/2')
+                    .send({});
+                expect(response.status).toEqual(400);
+            })
+            // should return a json object that contains an error message.
+            // should specify json as the content type in the http header.
+        })
+    })   
     
