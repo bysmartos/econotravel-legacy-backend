2db03829f8f4ee847e954a6cde53de42d695bc1a
nuria-barea <nuriab@gmail.com>
2022-07-18 15:50:34 +0200

    #10 role autorization implemented

--- server/src/route/userRoute.ts
+++ server/src/route/userRoute.ts
@@ -1,10 +1,11 @@
 import Router from 'express';
 import userController from '../controller/userController';
 import auth from '../middlewares/auth';
+import jwtHandler from '../middlewares/jwtHandler';
 const router = Router();
 
 router.post('/user/register', auth.encryptPassword, userController.saveUser);
-router.get('/user/all',userController.getAllUsers);
+router.get('/user/all',jwtHandler.validateTokenRole,userController.getAllUsers);
 router.post('/user/login',auth.validateUser,userController.login);
 
 export default router;
